dd10b31138cf02ff7b2b4d96b6496905ad378349
pythoninrelay <S4F4gEEnzT#zxy6Eos>
2020-06-15 13:05:34 +0300

    Add states

--- Assets/AdventureGame.cs
+++ Assets/AdventureGame.cs
@@ -1,13 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class AdventureGame : MonoBehaviour
 {
+   [SerializeField] Text textComponent;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        textComponent.text = "You have woken up from what feels like a long coma";
+
     }
 
     // Update is called once per frame
